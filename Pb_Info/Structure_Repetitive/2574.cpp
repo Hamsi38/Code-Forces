bc24a548c24d315be3bb83d9c36693f304c9deaf
Mehmet Akif Kabasakal <111529801+Hamsi38@users.noreply.github.com>
2025-12-29 02:44:39 +0200

    Implement solution for problem ID 2574 and remove temporary code runner file

--- Pb_Info/Structure_Repetitive/2574.cpp
+++ Pb_Info/Structure_Repetitive/2574.cpp
@@ -6,7 +6,44 @@ int main(){
 
     int n,m;
     cin>>n>>m;
+    if (n==1 && m==1)
+    {
+        cout<<"__0_0___";
+    }
+    else if (n==1 && m!=1)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            cout<<"__0_0___";
+        }
+        
+    }
+    
+    else
+    {
+        for (int i = 0; i < m; i++)
+        {
+            cout<<"__0_0___";
+        }
+        cout<<endl;
+        for (int i = 0; i < n-2; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                cout<<"_0___0__";
+            }
+            cout<<endl;
+        }
+        for (int i = 0; i < m; i++)
+        {
+            cout<<"0_____0_";
+        }
+        
+    }
     
 
+    
+
+    
     return 0;
 }
