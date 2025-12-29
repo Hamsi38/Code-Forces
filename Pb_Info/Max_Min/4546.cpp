a588e27e085363b0a9f90ee41cea71aa89fb6c40
Mehmet Akif Kabasakal <111529801+Hamsi38@users.noreply.github.com>
2025-12-29 02:41:08 +0200

    i solved a problem from pbinfo, ID 2574

--- Pb_Info/Max_Min/4546.cpp
+++ Pb_Info/Max_Min/4546.cpp
@@ -8,36 +8,12 @@ int main(){
 
     int a;
     cin>>a;
-    vector <int> vec;
 
+    long long nrmin = -999999999, nrmax = 999999999, cnt = 0;
 
-    if (a!=0)
-    {
-        long long nrmax;
-        vec.push_back(a);
-        while (a>0)
-        {
-            cin>>a;
-            if(a==0) break;
-            vec.push_back(a);
-        }
-        sort(vec.begin(),vec.end());
-        int cnt = vec.size();
-        int con = 0;
-        for (int i = cnt-1; i > -1; i--)
-        {
-            if(vec[cnt-1]!=vec[i])
-            {
-                nrmax = (long long)(vec[cnt-1]*vec[i]);
-                cout<<nrmax;
-                break;
-            }
-        }
-        
-    }
-    else
-    {
-        cout<<"NU EXISTA";
+    while (a--)
+    {   
+        cin>>
     }
     
     
